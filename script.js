84f698b361e432ea0dc5722268f667501bac505c
sevould <sevould6@gmail.com>
2026-06-21 22:16:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fitur Smooth Scroll untuk Navigasi
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        document.querySelector(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
